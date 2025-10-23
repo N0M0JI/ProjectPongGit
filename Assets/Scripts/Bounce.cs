6d971f9aa9c14c124a6a9c374ecc9d9b9327d65f
ecnag <phamgance@gmail.com>
2025-10-23 16:21:59 +0200

    Patch 0.2 - SPong
    -Fixed balls clipping throughwalls
    -Added sponges
    -Improved visuals
    -Added sounds

--- Assets/Scripts/Bounce.cs
+++ Assets/Scripts/Bounce.cs
@@ -3,16 +3,24 @@ using UnityEngine;
 public class Bounce : MonoBehaviour
 {
     public Rigidbody2D rb;
-    public Vector2 baseDir;
+    public Vector2 dir;
+    public float speed;
+    public AudioSource audio;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        ContactPoint2D contact = collision.GetContact(0);
+        dir = Vector2.Reflect(dir, contact.normal);
+        audio.Play();
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.linearVelocity = baseDir * 0.9f;
+        rb.linearVelocity = dir * speed;
     }
 }
